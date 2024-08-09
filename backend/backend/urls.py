ce7c681e2f239eb0d97686b30bb5fec4d3944a54
F1ower <Daniil.shirkin005@gmail.com>
2024-08-09 20:33:54 +0300

    123

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -1,15 +1,16 @@
+
 from django.urls import include, path
 from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
 from django.views.generic import TemplateView
 from django.urls import re_path
-#from viewapp.views import *
+from viewapp.views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('mainapp.urls')),
-    #re_path(r'^(?!admin|api/).*$', mainview),  # Обрабатывает все остальные пути
+    re_path(r'^(?!admin|api/).*$', mainview),  # Обрабатывает все остальные пути
 ]
 
 
